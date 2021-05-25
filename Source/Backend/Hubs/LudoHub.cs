2815c3c82e6c64def1b391813d1efb3f950ea9c2
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2021-05-25 16:13:53 +0200

    main hub for sending game data to clients

---
+++ Source/Backend/Hubs/LudoHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Backend.Hubs
+{
+    public class LudoHub : Hub
+    {
+        public async Task SendDiceRoll(int roll)
+        {
+            await base.Clients.All.SendAsync("RecieveDiceRoll", roll);
+        }
+    }
+}
