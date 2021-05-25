bebbe8933a9109fe43a157904ebadb3368aff6bb
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2021-05-25 16:14:39 +0200

    script file for communication with the ludohub

---
+++ Source/Frontend/wwwroot/js/Ludo.js
@@ -0,0 +1,20 @@
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl('https://localhost:44354', {
+        skipNegotiation: true,
+        transport: signalR.HttpTransportType.WebSockets
+    })
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+async function connect() {
+    try {
+        await connection.start();
+        console.log("SignalR connection established.");
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+connection.on("recieveDiceRoll", function (num) {
+    document.getElementById("rollInfo").innerText = "Player rolled " + num + "!";
+});
